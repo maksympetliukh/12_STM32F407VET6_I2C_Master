44b556ea65ffb017c3795ad6c15c38aa1e18f804
maksympetliukh <maxpetliukh@gmail.com>
2026-03-03 18:17:02 +0100

    first

---
+++ Inc/i2c.h
@@ -0,0 +1,15 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Mar 3, 2026
+ *      Author: maksym
+ */
+
+#include "stm32f407xx.h"
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+#endif /* I2C_H_ */
---
+++ Src/i2c.c
@@ -0,0 +1,8 @@
+/*
+ * i2c.c
+ *
+ *  Created on: Mar 3, 2026
+ *      Author: maksym
+ */
+
+
